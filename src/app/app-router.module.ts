5f577ac450b724e826126161ee278b5d88060600
Noshir Patel <noshirpatel86@gmail.com>
2021-08-13 21:29:39 +0530

    Lazy Loading Implemented

--- src/app/app-router.module.ts
+++ src/app/app-router.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
+import { AuthGuard } from './user/auth.guard';
 
 import { WelcomeComponent } from './home/welcome.component';
 import { PageNotFoundComponent } from './page-not-found.component';
 
 const ROUTE = [  
     {path:'home', component: WelcomeComponent},
+    {
+      path:'products',
+      canLoad:[AuthGuard],
+      loadChildren: () =>
+      import('./products/product.module')
+      .then(m => m.ProductModule)
+    },
     {path:'', redirectTo:'home', pathMatch:'full'},
     {path:'**', component: PageNotFoundComponent},  
 ]
